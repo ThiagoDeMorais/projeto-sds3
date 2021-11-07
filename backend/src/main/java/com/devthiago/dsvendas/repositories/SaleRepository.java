4630d3442df7011a7bf20143ba33bbdd1ddc0ff8
Thigo De Morais <demoraisthiago63@gmail.com>
2021-11-07 16:25:32 -0300

    Paginação

---
+++ backend/src/main/java/com/devthiago/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devthiago.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devthiago.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
